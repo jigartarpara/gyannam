e9a265980b8b1fca71e2fc77c3540a1c4c2d394c
Daizy <modidaizy6730@gmail.com>
2021-08-11 14:24:35 +0530

    fix new portal page for course schedule

--- gyannam/templates/pages/course_schedule.py
+++ gyannam/templates/pages/course_schedule.py
@@ -9,9 +9,10 @@ def get_context(context):
         context.course_schedule = course_schedule
     else:
         student = frappe.db.get_value("Student", {'student_email_id': frappe.session.user}, 'name')
-        students_group_list = frappe.db.get_all("Student Group", filters = [["Student Group Student", "student", "=", student]])
-        for schedule in students_group_list:
-            course_schedule = frappe.get_list('Course Schedule', filters={'student_group': schedule.name}, fields=['name', 'student_group', 'from_time', 'to_time', 'message_body'])
-        context.course_schedule = course_schedule
-
-
+        if student:
+            students_group_list = frappe.db.get_all("Student Group", filters = [["Student Group Student", "student", "=", student]])
+            group_list = []
+            for group in students_group_list:
+                group_list.append(group.name)
+            course_schedule = frappe.get_list('Course Schedule', filters= [["Course Schedule",'student_group','in', group_list]], fields=['name', 'student_group', 'from_time', 'to_time', 'message_body'])
+            context.course_schedule = course_schedule
