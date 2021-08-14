333e2b72204519d5b654f6a0f2eb96b875179171
Daizy <modidaizy6730@gmail.com>
2021-08-14 17:24:35 +0530

    Filter records on course schedule custom page

--- gyannam/templates/pages/course_schedule.py
+++ gyannam/templates/pages/course_schedule.py
@@ -2,11 +2,17 @@ from __future__ import unicode_literals
 import frappe
 
 def get_context(context):
-    context.no_cache = True
-    context.login_required = True
+    try:
+        schedule_date = frappe.request.args['schedule_date']
+    except:
+        schedule_date = ""
     if frappe.session.user == 'Administrator':
-        course_schedule = frappe.get_list('Course Schedule', fields=['name', 'schedule_date', 'from_time', 'to_time', 'live_class'])
-        context.course_schedule = course_schedule
+        if schedule_date:
+            course_schedule = frappe.get_list('Course Schedule', filters={'schedule_date': schedule_date}, fields=['name', 'schedule_date', 'from_time', 'to_time', 'live_class'])
+            context.course_schedule = course_schedule
+        else:
+            course_schedule = frappe.get_list('Course Schedule', fields=['name', 'schedule_date', 'from_time', 'to_time', 'live_class'])
+            context.course_schedule = course_schedule
     else:
         student = frappe.db.get_value("Student", {'student_email_id': frappe.session.user}, 'name')
         emp = frappe.db.get_value("Employee", {'prefered_email': frappe.session.user}, 'name')
@@ -16,12 +22,20 @@ def get_context(context):
             group_list = []
             for group in students_group_list:
                 group_list.append(group.name)
-            course_schedule = frappe.get_list('Course Schedule', filters= [["Course Schedule",'student_group','in', group_list]], fields=['name', 'schedule_date', 'from_time', 'to_time', 'live_class'])
-            context.course_schedule = course_schedule
+            if schedule_date:
+                course_schedule = frappe.get_list('Course Schedule', filters= [["Course Schedule",'student_group','in', group_list],["Course Schedule", "schedule_date","=",schedule_date]], fields=['name', 'schedule_date', 'from_time', 'to_time', 'live_class'])
+                context.course_schedule = course_schedule
+            else:
+                course_schedule = frappe.get_list('Course Schedule', filters= [["Course Schedule",'student_group','in', group_list]], fields=['name', 'schedule_date', 'from_time', 'to_time', 'live_class'])
+                context.course_schedule = course_schedule
         if instructor:
             instructor_group_list = frappe.db.get_all("Student Group", filters = [["Student Group Instructor", "instructor", "=", instructor]])
             group_list = []
             for group in instructor_group_list:
                 group_list.append(group.name)
-            course_schedule = frappe.get_list('Course Schedule', filters= [["Course Schedule",'student_group','in', group_list]], fields=['name', 'student_group', 'from_time', 'to_time', 'message_body'])
-            context.course_schedule = course_schedule
+            if schedule_date:
+                course_schedule = frappe.get_list('Course Schedule', filters= [["Course Schedule",'student_group','in', group_list],["Course Schedule", "schedule_date","=",schedule_date]], fields=['name', 'student_group', 'from_time', 'to_time', 'message_body'])
+                context.course_schedule = course_schedule
+            else:
+                course_schedule = frappe.get_list('Course Schedule', filters= [["Course Schedule",'student_group','in', group_list]], fields=['name', 'student_group', 'from_time', 'to_time', 'message_body'])
+                context.course_schedule = course_schedule
